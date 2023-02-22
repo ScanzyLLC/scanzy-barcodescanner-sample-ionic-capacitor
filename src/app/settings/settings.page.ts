15cafdeef81278008e48fdb368e9a3b1be62176c
Cindy <xinruxu007@gmail.com>
2023-02-22 11:32:58 +0800

    code format

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -8,26 +8,26 @@ import { ISettings, SettingsService } from '../services/settings.service';
 })
 export class SettingsPage implements OnInit {
   settings: ISettings;
-  check1DAll: boolean = false;
-  check2DAll: boolean = false;
-  constructor(public settingsService: SettingsService) { 
+  check1DAll = false;
+  check2DAll = false;
+  constructor(public settingsService: SettingsService) {
     this.settingsService.getSettings().then((value: ISettings)=>{
-      this.settings = value
-    })
+      this.settings = value;
+    });
   }
   updateSettings() {
-    this.settingsService.updateSettings(this.settings)
+    this.settingsService.updateSettings(this.settings);
   }
   changeAllBarcodes(event, type) {
-    let isChecked = event.target.checked
+    const isChecked = event.target.checked;
     this.settings.barcode[type].map(item=>{
-      item.value = isChecked
-      return item
-    })
+      item.value = isChecked;
+      return item;
+    });
   }
   checkBarcode() {
     setTimeout(() => {
-      this.settingsService.updateSettings(this.settings)
+      this.settingsService.updateSettings(this.settings);
     }, 100);
   }
   ngOnInit() {
